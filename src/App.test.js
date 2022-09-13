6d648b2792e6caaf3b7c0abafb169c654d2ff251
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-13 19:40:46 +0530

    updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Testing Pattern: AAA (Arrange, Act, Assert)
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Arrange (A)
+import { render, screen } from '@testing-library/react'; // importing necessary tools from RTL
+import App from './App'; // Taking up the Comp for testing
+
+// test spec / test case 
+test('has Welcome text', () => {
+
+  // Act  (Optional)
+  render(<App />); 
+  const welcomeEl = screen.getByText('Welcome!'); // return element that has text 'Welcome'
+
+  // Assert (must) 
+  expect(welcomeEl).toBeInTheDocument();
 });
+
+
