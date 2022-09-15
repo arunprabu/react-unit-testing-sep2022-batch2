b46d9a0e0dc0c1da97d956fe9a3778c65581575b
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-15 19:23:35 +0530

    snapshot testing

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SocialMedia = ({name, followersCount}) => {
+
+  const [ country, setCountry] = useState('Canada');
+
+  const handleCountryChange = (event) => {
+    console.log(event.target.value);
+    setCountry(event.target.value);
+  }
 
   return (
     <div>
       <h2>Social Media</h2>
-      <h3>Testing Props and JSX</h3>
+      <h3>Testing Props, Events, States, JSX, CSS, Snapshot Testing!</h3>
       <p>Top Social Media: </p>
       <p data-testid="topMediaInfo">{name} with {followersCount} Followers</p>
+
+      <label htmlFor="countryInput">Enter Country Name</label>
+      <input type='text' placeholder='Enter Country' value={country} 
+      onChange={handleCountryChange} id="countryInput"/>
+      <p role='paragraph'>Visit Cognizant {country} website to know more</p>
+
     </div>
   )
 }
--- src/components/SocialMedia/SocialMedia.test.js
+++ src/components/SocialMedia/SocialMedia.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 import SocialMedia from "./SocialMedia";
 
 // group of related test specs -- TEST SUITE
@@ -10,6 +11,53 @@ describe('SocialMediaComponent', () => {
 
     const topMediaInfoEl = screen.getByTestId('topMediaInfo');
     expect(topMediaInfoEl.textContent).toBe('Instagram with 200K Followers');
+  });
+
+  it(`has an input element with placeholder 'Enter Country'`, () => {
+    render(<SocialMedia />);
+    const countryInputEl = screen.getByPlaceholderText('Enter Country');
+    expect(countryInputEl).toBeTruthy();
+  });
+
+  // testing if input el works with onChange event or not
+  it('should update country on change event', () => {
+    render(<SocialMedia />);
+
+    // find the element in which you have onChange event
+    // text boxes will have default role as textbox
+    const countryInputEl = screen.getByRole('textbox', { name: 'Enter Country Name'});
+
+    // having mock event obj 
+    const mockEvtObj1 = {
+      target: {
+        value: 'Australia'
+      }
+    }
+    // trigger / emit / fire the event thru pgm 
+    fireEvent.change(countryInputEl, mockEvtObj1);
+    expect(countryInputEl.value).toBe('Australia');
+    expect(screen.getByRole('paragraph').textContent).toBe('Visit Cognizant Australia website to know more')
+
+    const mockEvtObj2 = {
+      target: {
+        value: 'Japan'
+      }
+    }
+    // trigger / emit / fire the event thru pgm 
+    fireEvent.change(countryInputEl, mockEvtObj2);
+    expect(countryInputEl.value).toBe('Japan');
+    expect(screen.getByRole('paragraph').textContent).toBe('Visit Cognizant Japan website to know more')
+  });
+
+  it('has right SocialMedia comp snapshot', () => {
+    // to take a snapshot we need react-test-renderer // npm i react-test-renderer@17.0.2
+    // taking snapshot and also converting it to json.
+    // [RECOMMENDED]: Take snapshot with all possible props as well as props children
+    const snapshotJSON = renderer.create(<SocialMedia name='Instagram' followersCount='200K'/>).toJSON();
+    // let's assert with a matcher toMatchSnapshot() from jest
+    expect(snapshotJSON).toMatchSnapshot();
+
+
   });
 
 });
