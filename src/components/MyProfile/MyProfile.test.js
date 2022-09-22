125304953832f753197847ac91d664075af9f5c4
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-09-22 06:53:30 +0530

    context api testing updated

--- src/components/MyProfile/MyProfile.test.js
+++ src/components/MyProfile/MyProfile.test.js
@@ -3,6 +3,13 @@ import { AuthContext } from "../../contexts/AuthContext";
 import MyProfile from "./MyProfile"
 
 describe('MyProfileComponent', () => {
+
+  const customRender = (ui, {providerProps, ...renderOptions}) => {
+    return render(
+      <AuthContext.Provider value={providerProps}>{ui}</AuthContext.Provider>,
+      renderOptions,
+    )
+  }
   
   test('shows pls login when not receiving auth context', () => {
     render(<MyProfile />)
@@ -12,7 +19,6 @@ describe('MyProfileComponent', () => {
   })
 
   it('receives data from context provider', () => {
-
     const auth = {
       isLoggedIn: true,
       username: 'johnw',
@@ -37,14 +43,24 @@ describe('MyProfileComponent', () => {
   })
 
 
+  // another way
+  it('receives auth info value from provider', () => {
+    const providerProps = {
+      isLoggedIn: true,
+      username: 'johnw',
+      fullName: 'John Williams',
+      lastLogin: '22/Sep/2022'
+    }
+    customRender(<MyProfile />, {providerProps})
+    expect(screen.getByText(/^Name:/).textContent).toBe(
+      'Name: John Williams'
+    )
+    expect(screen.getByText(/^Username:/).textContent).toBe(
+      'Username: johnw'
+    )
+    expect(screen.getByText(/^Last Login:/).textContent).toBe(
+      'Last Login: 22/Sep/2022'
+    )
+  })
+
 });
-
-
-
-// test('NameConsumer shows value from provider', () => {
-//   const providerProps = {
-//     value: 'C3PO',
-//   }
-//   customRender(<NameConsumer />, {providerProps})
-//   expect(screen.getByText(/^My Name Is:/)).toHaveTextContent('My Name Is: C3P0')
-// })
